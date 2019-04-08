efad8ab1b1a004e249501678e7d757dbc40ba16c
yf <291323003@qq.com>
2019-04-08 17:14:30 +0800

    up

---
+++ src/ActorProxyProcess.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yf
+ * Date: 2019-04-08
+ * Time: 17:11
+ */
+
+namespace EasySwoole\Actor;
+
+
+use EasySwoole\Component\Process\AbstractProcess;
+use Swoole\Coroutine\Socket;
+
+class ActorProxyProcess extends AbstractProcess
+{
+
+    public function run($arg)
+    {
+        // TODO: Implement run() method.
+        $socket = new Socket(AF_INET,SOCK_STREAM,0);
+        $socket->setOption(SOL_SOCKET,SO_REUSEPORT,true);
+        $socket->setOption(SOL_SOCKET,SO_REUSEADDR,true);
+        $socket->bind($arg->getListenHost(),$arg->getListenPort());
+        $socket->listen($arg->getBacklog());
+        while (1){
+            $client = $socket->accept(-1);
+            if($client){
+                go(function ()use($client){
+
+                });
+            }
+        }
+
+    }
+
+    public function onShutDown()
+    {
+        // TODO: Implement onShutDown() method.
+    }
+
+    public function onReceive(string $str)
+    {
+        // TODO: Implement onReceive() method.
+    }
+}
